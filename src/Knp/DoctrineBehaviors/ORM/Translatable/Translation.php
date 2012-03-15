523fbccad6297ca70abd5a54098329268efe4014
everzet <ever.zet@gmail.com>
2012-03-15 11:35:22 +0100

    added missing translation trait methods

--- src/Knp/DoctrineBehaviors/ORM/Translatable/Translation.php
+++ src/Knp/DoctrineBehaviors/ORM/Translatable/Translation.php
@@ -6,5 +6,41 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 trait Translation
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $locale;
+
+    /**
+     * Will be mapped to translatable entity
+     * by TranslatableListener
+     */
     protected $translatable;
+
+    public function __construct($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * Get id.
+     *
+     * @return id.
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
 }
