3d613293649f217508d11f091585debb4a678b00
everzet <ever.zet@gmail.com>
2012-03-15 11:34:57 +0100

    added missing Translatable methods

--- src/Knp/DoctrineBehaviors/ORM/Translatable/Translatable.php
+++ src/Knp/DoctrineBehaviors/ORM/Translatable/Translatable.php
@@ -6,5 +6,32 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 trait Translatable
 {
+    /**
+     * Will be mapped to translatable entity
+     * by TranslatableListener
+     */
     protected $translations;
+
+    public function getTranslations()
+    {
+        return $this->translations = $this->translations ?: new ArrayCollection();
+    }
+
+    public function translate($locale)
+    {
+        $translations = $this->getTranslations()->filter(function($translation) use ($locale) {
+            return $locale === $translation->getLocale();
+        });
+
+        if (count($translations)) {
+            return $translations[0];
+        }
+
+        $class       = get_class($this).'Translation';
+        $translation = new $class($locale);
+
+        $this->getTranslations()->add($translation);
+
+        return $translation;
+    }
 }
