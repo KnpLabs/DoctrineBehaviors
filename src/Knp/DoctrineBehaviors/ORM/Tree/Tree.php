838176e08bc91da3c2eb2faac9181fea8462997c
Klein Florian <florian.klein@free.fr>
2012-03-16 12:23:16 +0100

    add php doc blocks

--- src/Knp/DoctrineBehaviors/ORM/Tree/Tree.php
+++ src/Knp/DoctrineBehaviors/ORM/Tree/Tree.php
@@ -6,6 +6,15 @@ use Doctrine\ORM\QueryBuilder;
 
 trait Tree
 {
+    /**
+     * Returns all root nodes
+     *
+     * @api
+     *
+     * @param string $rootAlias
+     *
+     * @return array
+     */
     public function getRootNodes($rootAlias = 't')
     {
         $class         = $this->getClassName();
@@ -19,18 +28,21 @@ trait Tree
         return $qb->getQuery()->execute();
     }
 
+    /**
+     * Returns a node hydrated with its children and parents
+     *
+     * @api
+     *
+     * @param string $path
+     * @param string $rootAlias
+     *
+     * @return NodeInterface a node
+     */
     public function getTree($path = '', $rootAlias = 't')
     {
         $results = $this->getFlatTree($path, $rootAlias);
 
-        if (!count($results)) {
-            return;
-        }
-
-        $root = $results[0];
-        $root->buildTree($results);
-
-        return $root;
+        return $this->buildTree($results);
     }
 
     public function getTreeExceptNodeAndItsChildrenQB($entity, $rootAlias = 't')
@@ -41,7 +53,34 @@ trait Tree
         ;
     }
 
-    public function getFlatTreeQB($path, $rootAlias = 't')
+    /**
+     * Extracts the root node and constructs a tree using flat resultset
+     *
+     * @param Iterable|array $results a flat resultset
+     *
+     * @return NodeInterface
+     */
+    protected function buildTree($results)
+    {
+        if (!count($results)) {
+            return;
+        }
+
+        $root = $results[0];
+        $root->buildTree($results);
+
+        return $root;
+    }
+
+    /**
+     * Constructs a query builder to get a flat tree, starting from a given path
+     *
+     * @param string $path
+     * @param string $rootAlias
+     *
+     * @return QueryBuilder
+     */
+    protected function getFlatTreeQB($path, $rootAlias = 't')
     {
         $qb = $this->createQueryBuilder($rootAlias)
             ->andWhere($rootAlias.'.path LIKE :path')
@@ -54,10 +93,21 @@ trait Tree
         return $qb;
     }
 
+    /**
+     * manipulates the lfat tree query builder before executing it.
+     * Override this method to customize the tree query
+     *
+     * @param QueryBuilder $qb
+     */
     protected function addFlatTreeConditions(QueryBuilder $qb)
     {
     }
 
+    /**
+     * Executes the flat tree query builder
+     *
+     * @return array the flat resultset
+     */
     public function getFlatTree($path, $rootAlias = 't')
     {
         return $this
