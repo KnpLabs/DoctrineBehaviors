53990a57e134cfd6793f37797629180a8d36f804
everzet <ever.zet@gmail.com>
2012-03-23 16:26:19 +0100

    cleaned translation listener

--- src/Knp/DoctrineBehaviors/ORM/Translatable/TranslatableListener.php
+++ src/Knp/DoctrineBehaviors/ORM/Translatable/TranslatableListener.php
@@ -1,17 +1,38 @@
 <?php
 
+/*
+ * This file is part of the KnpDoctrineBehaviors package.
+ *
+ * (c) KnpLabs <http://knplabs.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Knp\DoctrineBehaviors\ORM\Translatable;
 
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs,
     Doctrine\Common\EventSubscriber,
     Doctrine\ORM\Events;
 
+/**
+ * Translatable Doctrine2 listener.
+ *
+ * Provides mapping for translatable entities and their translations.
+ */
 class TranslatableListener implements EventSubscriber
 {
+    /**
+     * Adds mapping to the translatable and translations.
+     *
+     * @param LoadClassMetadataEventArgs $eventArgs The event arguments
+     */
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
     {
         $classMetadata = $eventArgs->getClassMetadata();
-        if (in_array('Knp\DoctrineBehaviors\ORM\Translatable\Translatable', $classMetadata->reflClass->getTraitNames())) {
+        $traitNames    = $classMetadata->reflClass->getTraitNames();
+
+        if (in_array('Knp\DoctrineBehaviors\ORM\Translatable\Translatable', $traitNames)) {
             $classMetadata->mapOneToMany([
                 'fieldName'    => 'translations',
                 'mappedBy'     => 'translatable',
@@ -19,22 +40,30 @@ class TranslatableListener implements EventSubscriber
                 'targetEntity' => $classMetadata->name.'Translation'
             ]);
         }
-        if (in_array('Knp\DoctrineBehaviors\ORM\Translatable\Translation', $classMetadata->reflClass->getTraitNames())) {
+
+        if (in_array('Knp\DoctrineBehaviors\ORM\Translatable\Translation', $traitNames)) {
             $classMetadata->mapManyToOne([
                 'fieldName'    => 'translatable',
                 'inversedBy'   => 'translations',
-                // remove "Translation" suffix:
                 'targetEntity' => substr($classMetadata->name, 0, -11)
             ]);
 
             $classMetadata->setPrimaryTable([
-                'uniqueConstraints' => [[ 'name' => $classMetadata->getTableName().'_unique_translation', 'columns' => ['translatable_id', 'locale' ] ]],
+                'uniqueConstraints' => [[
+                    'name'    => $classMetadata->getTableName().'_unique_translation',
+                    'columns' => ['translatable_id', 'locale' ]
+                ]],
             ]);
         }
     }
 
+    /**
+     * Returns hash of events, that this listener is bound to.
+     *
+     * @return array
+     */
     public function getSubscribedEvents()
     {
-        return array(Events::loadClassMetadata);
+        return [Events::loadClassMetadata];
     }
 }
