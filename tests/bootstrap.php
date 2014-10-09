74ba8061b2f05ab69f816a6e1233d5f69e0a3ae9
Grégoire Paris <postmaster@greg0ire.fr>
2014-10-10 00:12:34 +0200

    remove everything mongoDB-related
    There is nothing in src or tests that relates to MongoDb, so there should be
    nothing either in the test environment.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,10 +30,6 @@ Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
     VENDOR_PATH.'/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
 );
 
-Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
-    VENDOR_PATH.'/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php'
-);
-
 $reader = new \Doctrine\Common\Annotations\AnnotationReader();
 $reader = new \Doctrine\Common\Annotations\CachedReader($reader, new \Doctrine\Common\Cache\ArrayCache());
 $_ENV['annotation_reader'] = $reader;
