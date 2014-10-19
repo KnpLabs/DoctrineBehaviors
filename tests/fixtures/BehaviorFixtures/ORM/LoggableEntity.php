4daf783325e5bffe23e578c40cd25fdf2714d94d
Krystian Kuczek <krystian@hexmedia.pl>
2014-10-20 00:46:25 +0200

    Test for DateTime format and arrays.

--- tests/fixtures/BehaviorFixtures/ORM/LoggableEntity.php
+++ tests/fixtures/BehaviorFixtures/ORM/LoggableEntity.php
@@ -25,6 +25,16 @@ class LoggableEntity
     private $title;
 
     /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $roles;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    /**
      * Returns object id.
      *
      * @return integer
@@ -37,7 +47,7 @@ class LoggableEntity
     /**
      * Get title.
      *
-     * @return title.
+     * @return string.
      */
     public function getTitle()
     {
@@ -47,10 +57,60 @@ class LoggableEntity
     /**
      * Set title.
      *
-     * @param title the value to set.
+     * @param $title the value to set.
      */
     public function setTitle($title)
     {
         $this->title = $title;
     }
+
+    /**
+     * Get Roles
+     *
+     * @return mixed
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set roles
+     *
+     * @param array $roles
+     *
+     * @return $this;
+     */
+    public function setRoles(array $roles = null)
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set date
+     *
+     * @param mixed $date
+     *
+     * @return $this;
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+
 }
