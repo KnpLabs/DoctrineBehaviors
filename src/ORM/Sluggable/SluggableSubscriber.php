568ec40e35d9fedf2b5b14d8fd3f3a302739928b
Victor <bocharsky.bw@gmail.com>
2015-07-29 23:54:32 +0300

    Remove unnecessary part of code

--- src/ORM/Sluggable/SluggableSubscriber.php
+++ src/ORM/Sluggable/SluggableSubscriber.php
@@ -6,48 +6,33 @@
 
 namespace Knp\DoctrineBehaviors\ORM\Sluggable;
 
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Knp\DoctrineBehaviors\Model\Sluggable\SluggableInterface;
-use Knp\DoctrineBehaviors\Reflection\ClassAnalyzer;
-
-use Knp\DoctrineBehaviors\ORM\AbstractSubscriber;
-
-use Doctrine\ORM\Event\LoadClassMetadataEventArgs,
-    Doctrine\Common\EventSubscriber,
-    Doctrine\ORM\Events,
-    Doctrine\ORM\Mapping\ClassMetadata;
 
 /**
  * Sluggable subscriber.
  *
  * Adds mapping to sluggable entities.
  */
-class SluggableSubscriber extends AbstractSubscriber
+class SluggableSubscriber implements EventSubscriber
 {
-    private $sluggableTrait;
-
-    public function __construct(ClassAnalyzer $classAnalyzer, $isRecursive, $sluggableTrait)
-    {
-        parent::__construct($classAnalyzer, $isRecursive);
-
-        $this->sluggableTrait = $sluggableTrait;
-    }
-
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
     {
-        $entity = $eventArgs->getEntity();
         $classMetadata = $eventArgs->getClassMetadata();
 
         if (null === $classMetadata->reflClass) {
             return;
         }
 
-        if ($entity instanceof SluggableInterface) {
+        if (is_subclass_of($classMetadata->getName(), 'Knp\DoctrineBehaviors\Model\Sluggable\SluggableInterface')) {
             if (!$classMetadata->hasField('slug')) {
                 $classMetadata->mapField(array(
                     'fieldName' => 'slug',
                     'type'      => 'string',
-                    'nullable'  => true
+                    'nullable'  => true,
                 ));
             }
         }
@@ -74,21 +59,5 @@ class SluggableSubscriber extends AbstractSubscriber
     public function getSubscribedEvents()
     {
         return [ Events::loadClassMetadata, Events::prePersist, Events::preUpdate ];
-    }
-
-    /**
-     * Checks if entity is sluggable
-     *
-     * @param ClassMetadata $classMetadata The metadata
-     *
-     * @return boolean
-     */
-    private function isSluggable(ClassMetadata $classMetadata)
-    {
-        return $this->getClassAnalyzer()->hasTrait(
-            $classMetadata->reflClass,
-            $this->sluggableTrait,
-            $this->isRecursive
-        );
     }
 }
