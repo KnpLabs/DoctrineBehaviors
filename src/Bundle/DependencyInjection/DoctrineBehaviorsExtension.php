3a916a548c19ea6bf29c1d5c86b1dabe7074700e
Albin Kerouaton <albin.kerouanton@knplabs.com>
2015-11-17 10:08:47 +0100

    Make Behaviors configurable.
    You can now enable/disable behaviors you don't want using the bundle config.
    By default, all behaviors are enabled. As soon as `knp_doctrine_behaviors` is
    specified, all behaviors are disabled and the developer need to specify which
    ones he want to enable.
    
    This fix conflict issues, for example when a `setCurrentLocale` method is
    defined by another third-party library/bundle.
    
    This way does not introduce BC breaks, but only a new feature. The best would
    be to add some interfaces and use them in subscribers. But that would imply
    a major release... Maybe for the next one :-)

--- src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
+++ src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
@@ -3,6 +3,7 @@
 namespace Knp\DoctrineBehaviors\Bundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -16,5 +17,22 @@ class DoctrineBehaviorsExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../../config'));
         $loader->load('orm-services.yml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        foreach ($config as $behavior => $enabled) {
+            if (!$enabled) {
+                $container->removeDefinition(sprintf('knp.doctrine_behaviors.%s_subscriber', $behavior));
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAlias()
+    {
+        return 'knp_doctrine_behaviors';
     }
 }
--- src/Bundle/DoctrineBehaviorsBundle.php
+++ src/Bundle/DoctrineBehaviorsBundle.php
@@ -2,9 +2,13 @@
 
 namespace Knp\DoctrineBehaviors\Bundle;
 
+use Knp\DoctrineBehaviors\Bundle\DependencyInjection\DoctrineBehaviorsExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineBehaviorsBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new DoctrineBehaviorsExtension();
+    }
 }
