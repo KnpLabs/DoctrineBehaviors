011d21747e4fbaad620143747dc985a2fcf5e046
pythagore <pierre.gori@gmail.com>
2016-11-10 18:09:36 +0100

    Create Uuidable trait

---
+++ src/Model/Uuidable/Uuidable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Reusable;
+
+use Ramsey\Uuid;
+
+/**
+ * Uuidable trait.
+ *
+ * Should be used inside entity, that needs to have an UUID4 for example for an API use
+ */
+trait Uuidable
+{
+    /**
+     * @var string
+     */
+    private $uuid;
+
+    /**
+     * Initialize the Uuid
+     */
+    private function initializeUuid()
+    {
+        $this->uuid = Uuid::uuid4();
+
+        return $this;
+    }
+
+    /**
+     * Set Uuid
+     *
+     * @param string $uuid
+     * @return $this
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    /**
+     * Get Uuid
+     *
+     * @return string
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function generateUuid()
+    {
+        return Uuid::Uuid4();
+    }
+}
