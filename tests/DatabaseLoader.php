b929313fcc440c0919c24777874037cefee9e407
TomasVotruba <tomas.vot@gmail.com>
2019-12-13 15:16:32 +0100

    use entity list instead of explicit

--- tests/DatabaseLoader.php
+++ tests/DatabaseLoader.php
@@ -7,21 +7,6 @@ namespace Knp\DoctrineBehaviors\Tests;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\SchemaTool;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\BlameableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\FilterableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\GeocodableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\LoggableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\SluggableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\SluggableMultiEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\SoftDeletableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\TimestampableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\Translatable\ExtendedTranslatableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\Translatable\ExtendedTranslatableEntityTranslation;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\TranslatableEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\TranslatableEntityTranslation;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\TreeNodeEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\UserEntity;
-use Knp\DoctrineBehaviors\Tests\Fixtures\Entity\UuidableEntity;
 
 final class DatabaseLoader
 {
@@ -40,24 +25,12 @@ final class DatabaseLoader
 
     public function reload(): void
     {
-        $entityClasses = [
-            SluggableEntity::class,
-            FilterableEntity::class,
-            GeocodableEntity::class,
-            LoggableEntity::class,
-            SluggableMultiEntity::class,
-            SoftDeletableEntity::class,
-            TimestampableEntity::class,
-            TranslatableEntity::class,
-            TranslatableEntityTranslation::class,
-            BlameableEntity::class,
-            UserEntity::class,
-            TreeNodeEntity::class,
-            UuidableEntity::class,
-            // translatable
-            ExtendedTranslatableEntity::class,
-            ExtendedTranslatableEntityTranslation::class,
-        ];
+        $allMetadata = $this->entityManager->getMetadataFactory()->getAllMetadata();
+
+        $entityClasses = [];
+        foreach ($allMetadata as $classMetadata) {
+            $entityClasses[] = $classMetadata->getName();
+        }
 
         $this->reloadEntityClasses($entityClasses);
     }
