bf7d5cb364b0f9f1dff6f7ef08b68dfd86e60aa6
TomasVotruba <tomas.vot@gmail.com>
2019-12-19 22:12:30 +0100

    cleanup

--- src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
+++ src/Bundle/DependencyInjection/DoctrineBehaviorsExtension.php
@@ -6,13 +6,17 @@ namespace Knp\DoctrineBehaviors\Bundle\DependencyInjection;
 
 use Doctrine\Common\EventSubscriber;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class DoctrineBehaviorsExtension extends Extension
 {
+    /**
+     * @var string
+     */
+    private const DOCTRINE_EVENT_SUBSCRIBER_TAG = 'doctrine.event_subscriber';
+
     /**
      * @param string[] $configs
      */
@@ -22,26 +26,7 @@ final class DoctrineBehaviorsExtension extends Extension
         $loader->load('services.yaml');
 
         // @see https://github.com/doctrine/DoctrineBundle/issues/674
-        $eventSubscriberAutoconfiguratoin = $this->findEventSubscriberAutoconfiguration($containerBuilder);
-
-        if ($eventSubscriberAutoconfiguratoin) {
-            $eventSubscriberAutoconfiguratoin->addTag('doctrine.event_subscriber');
-        } else {
-            $containerBuilder->registerForAutoconfiguration(EventSubscriber::class)
-                ->addTag('doctrine.event_subscriber');
-        }
-    }
-
-    private function findEventSubscriberAutoconfiguration(ContainerBuilder $containerBuilder): ?ChildDefinition
-    {
-        foreach ($containerBuilder->getAutoconfiguredInstanceof() as $type => $autoconfiguredInstanceOf) {
-            if ($type !== EventSubscriber::class) {
-                continue;
-            }
-
-            return $autoconfiguredInstanceOf;
-        }
-
-        return null;
+        $containerBuilder->registerForAutoconfiguration(EventSubscriber::class)
+            ->addTag(self::DOCTRINE_EVENT_SUBSCRIBER_TAG);
     }
 }
