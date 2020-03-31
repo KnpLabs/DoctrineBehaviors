459fd2fd344482af25f1336ac73981980bdbbe01
Soroka Anton <uginroot@gmail.com>
2020-03-31 15:01:19 +0600

    Fix cs-checker error

--- src/Bundle/DoctrineBehaviorsBundle.php
+++ src/Bundle/DoctrineBehaviorsBundle.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Knp\DoctrineBehaviors\Bundle;
 
+use \Knp\DoctrineBehaviors\DoctrineBehaviorsBundle as NewDoctrineBehaviorsBundle;
+
 /**
  * @deprecated 2.0.2
  */
-final class DoctrineBehaviorsBundle extends \Knp\DoctrineBehaviors\DoctrineBehaviorsBundle
+final class DoctrineBehaviorsBundle extends NewDoctrineBehaviorsBundle
 {
 }
