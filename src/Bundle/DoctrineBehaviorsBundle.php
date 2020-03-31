88a20f7de35587ee955eadc725d9a5ba8415fdfd
Soroka Anton <uginroot@gmail.com>
2020-03-31 14:37:30 +0600

    Fix broken old bandle register class

---
+++ src/Bundle/DoctrineBehaviorsBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Knp\DoctrineBehaviors\Bundle;
+
+/**
+ * @deprecated 2.0.2
+ */
+final class DoctrineBehaviorsBundle extends \Knp\DoctrineBehaviors\DoctrineBehaviorsBundle
+{
+}
--- src/DoctrineBehaviorsBundle.php
+++ src/DoctrineBehaviorsBundle.php
@@ -8,7 +8,7 @@ use Knp\DoctrineBehaviors\Bundle\DependencyInjection\DoctrineBehaviorsExtension;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class DoctrineBehaviorsBundle extends Bundle
+class DoctrineBehaviorsBundle extends Bundle
 {
     public function getContainerExtension(): Extension
     {
