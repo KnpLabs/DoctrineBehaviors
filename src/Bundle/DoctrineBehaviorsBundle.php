92bd39bab67bdc8698c0db02679484a31029bba3
Soroka Anton <uginroot@gmail.com>
2020-04-01 15:20:12 +0600

    Fix broken old bandle register class (#544)
    * Fix broken old bandle register class
    
    * Fix cs-checker error
    
    * Fix use style
    
    * Add duplicate class name files to esc exclude_files

---
+++ src/Bundle/DoctrineBehaviorsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Knp\DoctrineBehaviors\Bundle;
+
+use Knp\DoctrineBehaviors\DoctrineBehaviorsBundle as NewDoctrineBehaviorsBundle;
+
+/**
+ * @deprecated 2.0.2
+ */
+final class DoctrineBehaviorsBundle extends NewDoctrineBehaviorsBundle
+{
+}
