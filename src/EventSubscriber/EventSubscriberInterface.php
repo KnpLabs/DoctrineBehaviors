63222b4d853a6091c3c02a65e2293949dee43bb6
Wim Winterberg <wim@we.are.builders>
2020-04-06 08:43:54 +0200

    Fix for "Coding Standard / ecs"

--- src/EventSubscriber/EventSubscriberInterface.php
+++ src/EventSubscriber/EventSubscriberInterface.php
@@ -8,8 +8,7 @@ use Doctrine\Common\EventSubscriber as BaseEventSubscriber;
 /**
  * Interface EventSubscriberInterface to use for auto configuration
  *
- * @author Enrico Thies <et@mandarin-medien.de>
- * @see    https://github.com/doctrine/DoctrineBundle/issues/674
+ * @see https://github.com/doctrine/DoctrineBundle/issues/674
  */
 interface EventSubscriberInterface extends BaseEventSubscriber
 {
