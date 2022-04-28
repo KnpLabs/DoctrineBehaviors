9f522e6f413a3679c5a803b655e58257078ab4c0
Tomas Votruba <tomas.vot@gmail.com>
2022-04-28 20:33:34 +0200

    bump rector.php to RectorConfig class

--- rector.php
+++ rector.php
@@ -2,23 +2,20 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
 use Rector\Nette\Set\NetteSetList;
 use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/utils']);
+    $rectorConfig->importNames();
+    $rectorConfig->parallel();
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/utils']);
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         RenamePropertyToMatchTypeRector::class => [__DIR__ . '/tests/ORM/'],
 
         UnionTypesRector::class => [
@@ -29,12 +26,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     // doctrine annotations to attributes
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
-
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::CODING_STYLE);
-    $containerConfigurator->import(NetteSetList::NETTE_CODE_QUALITY);
-    $containerConfigurator->import(SetList::NAMING);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+    $rectorConfig->sets([
+        DoctrineSetList::DOCTRINE_ORM_29,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        NetteSetList::NETTE_CODE_QUALITY,
+        SetList::NAMING,
+        LevelSetList::UP_TO_PHP_80,
+    ]);
 };
