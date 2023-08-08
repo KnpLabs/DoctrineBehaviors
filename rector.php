4cef745d0b9fff87ae8a9745212d48c92562d6ad
Thomas Lallement <thomas.lallement@9online.fr>
2023-08-08 14:28:49 +0200

    Update rector.php
    Remove missing NetteSetList
    
    [ERROR] Class "Rector\Nette\Set\NetteSetList" not found

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
-use Rector\Nette\Set\NetteSetList;
 use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -31,7 +30,6 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::DEAD_CODE,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
-        NetteSetList::NETTE_CODE_QUALITY,
         SetList::NAMING,
         LevelSetList::UP_TO_PHP_80,
     ]);
